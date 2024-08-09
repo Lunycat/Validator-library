c8c2bb6ec5147cfd62cf3e1f2aa6d826dc250352
Artem <lunycat@mail.ru>
2024-08-09 13:04:54 +0300

    add first test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    void testMain() {
+        assertEquals("a", "a");
+    }
+}
