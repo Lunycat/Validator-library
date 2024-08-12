799044e3b1e4b706b2c4741122358111e7f046a6
Artem <lunycat@mail.ru>
2024-08-12 18:16:17 +0300

    update code

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,11 +1,8 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public final class MapSchema extends BaseSchema<Map<String, String>> {
-
-    private final Map<String, BaseSchema<String>> mapRules = new HashMap<>();
 
     public MapSchema() {
         rules.put("required", map -> isBlank || map != null);
@@ -16,13 +13,9 @@ public final class MapSchema extends BaseSchema<Map<String, String>> {
         return this;
     }
 
-    public MapSchema shape(Map<String, BaseSchema<String>> map) {
-        for (String key : map.keySet()) {
-            rules.put("shape", value -> {
-                BaseSchema<String> bs = map.get(key);
-                String check = value.get(key);
-                return bs.isValid(check);
-            });
+    public MapSchema shape(Map<String, BaseSchema<String>> data) {
+        for (String key : data.keySet()) {
+            rules.put("shape", map -> data.get(key).isValid(map.get(key)));
         }
         return this;
     }
